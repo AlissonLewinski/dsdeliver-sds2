8e2eb8c0ea3b1a6542f2b18fe8961ab012f76fa0
Alisson <lucasali7@gmail.com>
2021-01-04 20:58:48 -0300

    Project created

---
+++ backend/src/main/java/com/aliski/lissdeliver/LissdeliverApplication.java
@@ -0,0 +1,13 @@
+package com.aliski.lissdeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LissdeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LissdeliverApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/aliski/lissdeliver/LissdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aliski.lissdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LissdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
