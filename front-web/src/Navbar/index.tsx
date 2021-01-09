5b5b19cafe43bf4854c015c2da105cb59afda9c8
Alisson Lewinski <lucasali7@gmail.com>
2021-01-09 02:58:46 -0300

    Add routes and StepsHeader

--- front-web/src/Navbar/index.tsx
+++ front-web/src/Navbar/index.tsx
@@ -1,10 +1,11 @@
 import './styles.css'
 import { ReactComponent as Logo } from './logo.svg'
+import { Link } from 'react-router-dom'
 function Navbar() {
     return (
         <nav className="main-navbar">
             <Logo className="logo"/>
-            <a href="home" className="logo-text">Liss Delivery (DS Delivery)</a>
+            <Link to="/" className="logo-text">Liss Delivery (DS Delivery)</Link>
         </nav>
     )
 }
