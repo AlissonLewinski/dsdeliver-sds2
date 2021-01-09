7c25eb4fd1726af4685a7c6d8aad04a8a5816fd6
Alisson Lewinski <lucasali7@gmail.com>
2021-01-09 17:04:07 -0300

    Add Products List and Search order location

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,9 +1,25 @@
-import StepsHeader from './StepsHeader'
+import { useEffect, useState } from 'react'
 import './styles.css'
+import ProductsList from './ProductsList'
+import StepsHeader from './StepsHeader'
+import { OrderLocationData, Product } from './types'
+import { fetchProducts } from '../api'
+import OrderLocation from './OrderLocation'
+
 function Orders() {
+    const [products, setProducts] = useState<Product[]>([])
+    const [orderLocation, setOrderLocation] = useState<OrderLocationData>()
+    useEffect(() => {
+        fetchProducts()
+            .then(res => setProducts(res.data))
+            .catch(err => console.log(err))
+    }, [])
+
     return (
         <div className="orders-container">
-            <StepsHeader />
+            <StepsHeader/>
+            <ProductsList products={products}/>
+            <OrderLocation onChangeLocation={location => setOrderLocation(location)}/>
         </div>
     )
 }
